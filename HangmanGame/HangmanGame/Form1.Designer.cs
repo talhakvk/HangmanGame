1792ee2f0e307fa0a053c45773c3950f03893de4
Talha Kavak <talhakavak2005@gmail.com>
2024-10-16 21:50:35 +0300

    HangmanGame

---
+++ HangmanGame/HangmanGame/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace HangmanGame
+{
+    partial class Form1
+    {
+        // Component field definitions
+        private DevExpress.XtraEditors.LabelControl labelHangman; // For Hangman graphic
+        private DevExpress.XtraEditors.LabelControl labelWordProgress; // For the word's progress
+        private DevExpress.XtraEditors.SimpleButton buttonGuess; // Guess button
+        private DevExpress.XtraEditors.TextEdit textBoxLetter; // Input field for letter guesses
+
+        // Layout and properties settings for the components on the form
+        private void InitializeComponent()
+        {
+            this.labelHangman = new DevExpress.XtraEditors.LabelControl();
+            this.labelWordProgress = new DevExpress.XtraEditors.LabelControl();
+            this.buttonGuess = new DevExpress.XtraEditors.SimpleButton();
+            this.textBoxLetter = new DevExpress.XtraEditors.TextEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxLetter.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelHangman
+            // 
+            this.labelHangman.Appearance.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold);
+            this.labelHangman.Appearance.Options.UseFont = true;
+            this.labelHangman.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelHangman.Location = new System.Drawing.Point(12, 12);
+            this.labelHangman.Name = "labelHangman";
+            this.labelHangman.Size = new System.Drawing.Size(300, 100);
+            this.labelHangman.TabIndex = 0;
+            this.labelHangman.Text = "Hangman";
+            // 
+            // labelWordProgress
+            // 
+            this.labelWordProgress.Appearance.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.labelWordProgress.Appearance.Options.UseFont = true;
+            this.labelWordProgress.Location = new System.Drawing.Point(12, 150);
+            this.labelWordProgress.Name = "labelWordProgress";
+            this.labelWordProgress.Size = new System.Drawing.Size(126, 28);
+            this.labelWordProgress.TabIndex = 1;
+            this.labelWordProgress.Text = "_ _ _ _ _ _";
+            // 
+            // buttonGuess
+            // 
+            this.buttonGuess.Location = new System.Drawing.Point(80, 200);
+            this.buttonGuess.Name = "buttonGuess";
+            this.buttonGuess.Size = new System.Drawing.Size(100, 23);
+            this.buttonGuess.TabIndex = 3;
+            this.buttonGuess.Text = "Guess";
+            this.buttonGuess.Click += new System.EventHandler(this.buttonGuess_Click);
+            // 
+            // textBoxLetter
+            // 
+            this.textBoxLetter.Location = new System.Drawing.Point(12, 200);
+            this.textBoxLetter.Name = "textBoxLetter";
+            this.textBoxLetter.Size = new System.Drawing.Size(50, 22);
+            this.textBoxLetter.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Controls.Add(this.buttonGuess);
+            this.Controls.Add(this.textBoxLetter);
+            this.Controls.Add(this.labelWordProgress);
+            this.Controls.Add(this.labelHangman);
+            this.Name = "Form1";
+            this.Text = "Hangman Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxLetter.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ HangmanGame/HangmanGame/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace HangmanGame
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        // Array holding the different stages of the hangman graphic
+        private string[] hangmanStages = new string[] 
+        {
+            @"
+               --------
+               |      |
+               |      
+               |    
+               |      
+               |     
+               -",
+            @"
+               --------
+               |      |
+               |      O
+               |    
+               |      
+               |     
+               -",
+            @"
+               --------
+               |      |
+               |      O
+               |      |
+               |      
+               |     
+               -",
+            @"
+               --------
+               |      |
+               |      O
+               |     \|
+               |      
+               |     
+               -",
+            @"
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      
+               |     
+               -",
+            @"
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      |
+               |     
+               -",
+            @"
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      |
+               |     / 
+               -",
+            @"
+               --------
+               |      |
+               |      O
+               |     \|/
+               |      |
+               |     / \
+               -"
+        };
+
+        // The player has a total of 8 guesses
+        private int remainingGuesses = 8; 
+        private string selectedWord; // The word to be guessed in the game
+        private List<char> enteredLetters = new List<char>(); // List of entered letters
+        private string[] guessProgress; // The current state of the word being guessed
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartGame(); // Start the game
+        }
+
+        // Method to start the game
+        private void StartGame()
+        {
+            // Select a random word
+            string[] words = new string[] { "CLASS", "OBJECT", "DATA", "UNITY", "BACKEND", "FRONTEND" };
+            Random random = new Random();
+            selectedWord = words[random.Next(0, words.Length)]; // A random word is selected
+            guessProgress = new string[selectedWord.Length]; // Create an empty guess state as long as the selected word
+
+            // Initialize with underscores for each letter in the word
+            for (int i = 0; i < guessProgress.Length; i++)
+            {
+                guessProgress[i] = "_";
+            }
+
+            // Reset the hangman graphic and the word progress
+            labelHangman.Text = hangmanStages[0]; // Initial hangman graphic
+            labelWordProgress.Text = string.Join(" ", guessProgress); // Show the word state as "_ _ _"
+            enteredLetters.Clear(); // Clear the entered letters
+            remainingGuesses = 8; // Reset remaining guesses
+        }
+
+        // Method executed when the guess button is clicked
+        private void buttonGuess_Click(object sender, EventArgs e)
+        {
+            string enteredLetterStr = textBoxLetter.Text.ToUpper(); // Convert entered letter to uppercase
+            if (enteredLetterStr.Length == 1) // If a single letter is entered
+            {
+                char enteredLetter = enteredLetterStr[0];
+
+                // If the letter was already entered, display a warning
+                if (enteredLetters.Contains(enteredLetter))
+                {
+                    XtraMessageBox.Show("You have already entered this letter, try another.");
+                    return;
+                }
+
+                // Add the entered letter to the list
+                enteredLetters.Add(enteredLetter);
+
+                // Check if the entered letter exists in the word
+                bool letterFound = false;
+                for (int i = 0; i < selectedWord.Length; i++)
+                {
+                    if (selectedWord[i] == enteredLetter)
+                    {
+                        guessProgress[i] = enteredLetter.ToString();
+                        letterFound = true;
+                    }
+                }
+
+                // If the letter is not found, decrease remaining guesses and update hangman graphic
+                if (!letterFound)
+                {
+                    remainingGuesses--;
+                    if (remainingGuesses > 0) // Check if any guesses remain
+                    {
+                        labelHangman.Text = hangmanStages[8 - remainingGuesses]; // Update hangman graphic after incorrect guesses
+                    }
+                }
+
+                // If no guesses remain, the game ends
+                if (remainingGuesses == 0)
+                {
+                    XtraMessageBox.Show($"Game Over! The word was: {selectedWord}"); // Notify the player of losing
+                    StartGame(); // Restart the game
+                }
+
+                // Update the current word state
+                labelWordProgress.Text = string.Join(" ", guessProgress);
+
+                // If all letters are guessed correctly, the player wins
+                if (string.Join("", guessProgress) == selectedWord)
+                {
+                    XtraMessageBox.Show("Congratulations, you won!"); // Notify the player of winning
+                    StartGame(); // Restart the game
+                }
+
+                // Check again if no guesses remain and notify the player of losing
+                if (remainingGuesses == 0)
+                {
+                    labelHangman.Text = hangmanStages[7]; // Show the final hangman graphic
+                    XtraMessageBox.Show($"Game Over! The word was: {selectedWord}"); // Notify of the lost game
+                    StartGame(); // Restart the game
+                }
+            }
+            else
+            {
+                XtraMessageBox.Show("Please enter a single letter!"); // Warn if more than one character is entered
+            }
+
+            textBoxLetter.Text = string.Empty; // Clear the input field
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
